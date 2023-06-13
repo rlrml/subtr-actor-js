a0b138eaba398bc8ef03d6672b6b57100e629569
Ivan Malison <IvanMalison@gmail.com>
2023-06-12 23:06:37 -0600

    bump version

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,7 @@
-use std::path::Path;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn get_json(filepath: &str) -> Result<String, JsValue> {
-    let path = Path::new(filepath);
-    let data = std::fs::read(path).map_err(|err| err.to_string())?;
-
+pub fn get_json(data: &[u8]) -> Result<String, JsValue> {
     let replay = boxcars::ParserBuilder::new(&data)
         .must_parse_network_data()
         .on_error_check_crc()
@@ -14,7 +10,7 @@ pub fn get_json(filepath: &str) -> Result<String, JsValue> {
 
     let data = subtr_actor::ReplayDataCollector::new()
         .get_replay_data(&replay)
-        .map_err(|_e| "fuck".to_string())?;
+        .unwrap();
 
     Ok(serde_json::to_string(&data).map_err(|err| err.to_string())?)
 }
